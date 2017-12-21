319f9dedc9980ac3a1eba5a63eab594f394e79d8
Semen Chudakov <css-99@i.ua>
2017-12-21 22:46:51 +0200

    [21.12.17] - mestakes in collections are corrected

--- test/com/sschudakov/parsing/WordsCollectionsReaderTest.java
+++ test/com/sschudakov/parsing/WordsCollectionsReaderTest.java
@@ -11,10 +11,14 @@ public class WordsCollectionsReaderTest {
 
     @Test
     public void readWordsCollectionsTest() {
+        int numOfWords = 0;
         for (HashMap<Word, List<Word>> wordListHashMap : WordsCollectionsReader.readWordsCollections()) {
+            numOfWords += wordListHashMap.keySet().size();
             for (Word word : wordListHashMap.keySet()) {
                 System.out.println("word: " + word + " meanings" + wordListHashMap.get(word));
             }
         }
+
+        System.out.println("num of words: " + numOfWords);
     }
 }
