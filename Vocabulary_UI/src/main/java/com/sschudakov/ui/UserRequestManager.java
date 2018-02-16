5acd02845c665d4fac3e0ff5ef5ee047cf80c4c4
AndreaB330 <Andrey.BorzyakR@gmail.com>
2018-02-17 00:12:14 +0200

    User requests manager. Without impl.

---
+++ Vocabulary_UI/src/main/java/com/sschudakov/ui/UserRequestManager.java
@@ -0,0 +1,69 @@
+package com.sschudakov.ui;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class UserRequestManager {
+
+    public UserRequestManager() {
+    }
+
+    //get requests
+    public List<String> getLanguages() {
+        return null;
+    }
+
+    public List<String> getCollections() {
+        return null;
+    }
+
+    public List<String> getClasses() {
+        return null;
+    }
+
+    public List<String> getMeaningsByWord(String word, String wordLanguage, String meaningsLanguage) {
+        return null;
+    }
+
+    public String getClassByWord(String word, String language) {
+        return null;
+    }
+
+    public List<String> getWordsByLanguageName(String language) {
+        return null;
+    }
+
+    public HashMap<String, Boolean> getCollectionsByWord(String word, String language) {
+        return null;
+    }
+
+    public List<String> getWordsByCollectionName(String collection) {
+        return null;
+    }
+
+    //change requests
+    public void addWord() {}
+
+    public void addCollection() {}
+
+    public void addLanguage() {}
+
+    public void addMeaningToWord(String word, String language, String meaning, String meaningLanguage) {};
+
+    public void addWordToCollection(String word, String language, String collection) {};
+
+    public void changeWordClassTo(String word, String language, String className) {};
+
+    //check requests
+    public boolean wordExists(String word, String language) {
+        return false;
+    }
+
+    public boolean collectionExists(String collection) {
+        return false;
+    }
+
+    public boolean languageExists(String language) {
+        return false;
+    }
+}
