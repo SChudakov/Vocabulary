fd1ab103527ad05e4f3acff8cc228bd7d9f8816b
Semen Chudakov <simai9946@gmail.com>
2018-03-02 16:58:05 +0200

    [2.03.12]: - renamed rom dao to jdbc dao - removed interfaces in service pair

--- Vocabulary_BL/src/main/java/com/sschudakov/service/LanguageSrv.java
+++ Vocabulary_BL/src/main/java/com/sschudakov/service/LanguageSrv.java
@@ -1,39 +1,37 @@
-package com.sschudakov.service.impl;
+package com.sschudakov.service;
 
-import com.sschudakov.dao.impl.ormlite.LanguageDaoOltImpl;
+import com.sschudakov.dao.impl.jdbc.LanguageDaoJdbcImpl;
 import com.sschudakov.dao.interf.LanguageDao;
 import com.sschudakov.entity.Language;
-import com.sschudakov.service.interf.LanguageSrv;
 
 import java.sql.SQLException;
 import java.util.List;
 
-public class LanguageSrvImpl implements LanguageSrv {
+public class LanguageSrv {
 
     private LanguageDao languageDao;
 
 
-    public LanguageSrvImpl() {
-        this.languageDao = new LanguageDaoOltImpl();
+    public LanguageSrv() {
+        this.languageDao = new LanguageDaoJdbcImpl();
     }
 
 
-    @Override
     public void create(String languageName) throws SQLException {
         this.languageDao.save(new Language(languageName));
     }
 
-    @Override
+
     public Language findById(Integer id) throws SQLException {
         return this.languageDao.findById(id);
     }
 
-    @Override
+
     public Language findByName(String name) throws SQLException {
         return this.languageDao.findByName(name);
     }
 
-    @Override
+
     public List<Language> findAll() throws SQLException {
         return this.languageDao.findAll();
     }
