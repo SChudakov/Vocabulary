f4eaad23f6710ccdc557659729b6c2530edbad0d
Semen Chudakov <simai9946@gmail.com>
2018-05-26 19:28:05 +0300

    added REST collections info response

--- src/main/java/com/sschudakov/web/controller/RESTController.java
+++ src/main/java/com/sschudakov/web/controller/RESTController.java
@@ -2,9 +2,11 @@ package com.sschudakov.web.controller;
 
 
 import com.sschudakov.dao.springdata.LanguageRepository;
+import com.sschudakov.dao.springdata.WordCollectionRepository;
 import com.sschudakov.dao.springdata.WordRepository;
 import com.sschudakov.entity.Language;
 import com.sschudakov.entity.Word;
+import com.sschudakov.entity.WordCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +22,14 @@ import java.util.Optional;
 public class RESTController {
 
     /*private final WordRepository wordRepository;
+    private final WordCollectionRepository wordCollectionRepository;
     private final LanguageRepository languageRepository;
 
     @Autowired
-    public RESTController(WordRepository wordRepository, LanguageRepository languageRepository) {
+    public RESTController(WordRepository wordRepository, LanguageRepository languageRepository, WordCollectionRepository wordCollectionRepository) {
         this.wordRepository = wordRepository;
         this.languageRepository = languageRepository;
+        this.wordCollectionRepository = wordCollectionRepository;
     }
 
     @RequestMapping(value = "/words/wordInfo", method = RequestMethod.GET)
@@ -52,7 +56,18 @@ public class RESTController {
     public ResponseEntity<List<Word>> getCollectionInfo(
             @RequestParam("collectionName") String collectionsName
     ) {
-        throw new UnsupportedOperationException();
+        ResponseEntity<List<Word>> result;
+
+        Optional<WordCollection> optionalWordCollection = this.wordCollectionRepository.getByCollectionName(collectionsName);
+
+        if (optionalWordCollection.isPresent()) {
+            List<Word> words = this.wordRepository.getByCollection(optionalWordCollection.get());
+            result = new ResponseEntity<>(words, HttpStatus.OK);
+        } else {
+            result = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return result;
     }
 
     @RequestMapping(value = "/languages/languageInfo", method = RequestMethod.GET)
